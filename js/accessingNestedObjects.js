cbb446e46329cac85845b1004870384049543982
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 00:11:49 +0300

    feat: add solution task Accessing Nested Objects

---
+++ js/accessingNestedObjects.js
@@ -0,0 +1,19 @@
+//Name of the task: Accessing Nested Objects
+
+//task: Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use dot notation for all properties where possible, otherwise use bracket notation.
+
+//solution:
+
+const myStorage = {
+	"car": {
+		"inside": {
+			"glove box": "maps",
+			"passenger seat": "crumbs"
+		},
+		"outside": {
+			"trunk": "jack"
+		}
+	}
+};
+
+const gloveBoxContents = myStorage.car.inside['glove box'];
