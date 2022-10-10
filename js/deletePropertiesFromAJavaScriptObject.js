ae8a505cdf3536052dcaa5e97a91251c14a8b42f
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-10 23:36:42 +0300

    feat: add solution task  Add New Properties to a JavaScript Object

---
+++ js/deletePropertiesFromAJavaScriptObject.js
@@ -0,0 +1,15 @@
+//Name of the task: Add New Properties to a JavaScript Object
+
+//task: Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
+
+//solution:
+
+const myDog = {
+	"name": "Happy Coder",
+	"legs": 4,
+	"tails": 1,
+	"friends": ["freeCodeCamp Campers"],
+	"bark": "woof"
+};
+
+delete myDog['tails']
