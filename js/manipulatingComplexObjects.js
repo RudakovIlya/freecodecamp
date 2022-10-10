3edc73e6d76a720583f461f8ed1bebb17a458e8d
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 00:07:36 +0300

    feat: add solution task Testing Objects for Properties

---
+++ js/manipulatingComplexObjects.js
@@ -0,0 +1,29 @@
+//Name of the task: Testing Objects for Properties
+
+//task: Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+//solution:
+
+const myMusic = [
+	{
+		"artist": "Billy Joel",
+		"title": "Piano Man",
+		"release_year": 1973,
+		"formats": [
+			"CD",
+			"8T",
+			"LP"
+		],
+		"gold": true
+	}, {
+		"artist": "Billy Jeen",
+		"title": "Piano Man",
+		"release_year": 228,
+		"formats": [
+			"CD",
+			"8T",
+			"LP"
+		],
+		"gold": true
+	}
+];
