c95d96efc1ba8b7ec9888aaa5a16c1ff7d40b939
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 19:19:39 +0300

    feat: add solution Iterate Odd Numbers With a For Loop

--- js/iterateOddNumbersWithAForLoop.js
+++ js/iterateOddNumbersWithAForLoop.js
@@ -3,3 +3,14 @@
 //task: Push the odd numbers from 1 through 9 to myArray using a for loop.
 
 //solution:
+
+// Setup
+const myArray = [];
+
+for (let i = 1; i < 10; i += 2) {
+
+	myArray.push(i)
+
+}
+
+// Only change code below this line
