c9c23b0cbb4f471ad609b7cbb89e698d0b6053e6
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 19:23:01 +0300

    feat: add task Count Backwards With a For Loop

---
+++ js/countBackwardsWithAForLoop.js
@@ -0,0 +1,5 @@
+//Name of the task: Count Backwards With a For Loop
+
+//task: Read the values of the properties an entree and the drink of testObj using bracket notation and assign them to entreeValue and drinkValue respectively.
+
+//solution:
