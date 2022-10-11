042878424ccbf5dd5862e429061ee56592f35a1c
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 20:36:34 +0300

    feat: add solution Replace Loops using Recursion

--- js/replaceLoopsUsingRecursion.js
+++ js/replaceLoopsUsingRecursion.js
@@ -1,5 +1,17 @@
 //Name of the task: Replace Loops using Recursion
 
-//task: Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
+//task: Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
 
 //solution:
+
+function sum(arr, n) {
+
+	if (n <= 0) {
+		return 0
+	}
+
+	else {
+		return sum(arr, n - 1) + arr[n - 1];
+	}
+
+}
