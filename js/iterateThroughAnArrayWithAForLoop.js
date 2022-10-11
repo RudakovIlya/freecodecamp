29ed2284ad1eb9a90b9c6238231970170daef865
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 19:35:52 +0300

    test

---
+++ js/iterateThroughAnArrayWithAForLoop.js
@@ -0,0 +1,7 @@
+//Name of the task: Iterate Through an Array with a For Loop
+
+//task: Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
+
+//solution:
+
+// Setup
