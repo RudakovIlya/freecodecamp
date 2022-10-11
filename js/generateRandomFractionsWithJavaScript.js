09a5ffc6c65aef977c38bc20c856a7b2a0ccfb9f
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 21:01:45 +0300

    feat: solution task Generate Random Fractions with JavaScript

--- js/generateRandomFractionsWithJavaScript.js
+++ js/generateRandomFractionsWithJavaScript.js
@@ -1,5 +1,11 @@
 //Name of the task: Generate Random Fractions with JavaScript
 
-//task: We have an array of objects representing different people in our contacts lists. A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you. The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.If both are true, then return the "value" of that property. If name does not correspond to any contacts then return the string No such contact. If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
+//task: Change randomFraction to return a random number instead of returning 0.
 
 //solution:
+
+function randomFraction() {
+
+	return Math.random()
+
+}
