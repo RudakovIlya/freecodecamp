935167b9b9a901e59b2a8e50b5b1a07e1b2d48e3
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 19:53:35 +0300

    feat: add task Replace Loops using Recursion

---
+++ js/replaceLoopsUsingRecursion.js
@@ -0,0 +1,5 @@
+//Name of the task: Replace Loops using Recursion
+
+//task: Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
+
+//solution:
