57a740a6a1730f8c5b32e2c33dd77eb348a6a1ed
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 19:30:23 +0300

    test

--- js/countBackwardsWithAForLoop.js
+++ js/countBackwardsWithAForLoop.js
@@ -2,4 +2,16 @@
 
 //task: Push the odd numbers from 9 through 1 to myArray using a for loop.
 
-//solution:
+//solution: 
+
+// Setup
+
+const myArray = [];
+
+for (let i = 9; i > 0; i -= 2) {
+
+	myArray.push(i)
+
+}
+
+
