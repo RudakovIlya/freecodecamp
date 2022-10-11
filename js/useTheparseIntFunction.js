6bf3708d551da124d23a37d4b5699ee3e5770313
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 21:12:04 +0300

    feat: add solution Use the parseInt Function

---
+++ js/useTheparseIntFunction.js
@@ -0,0 +1,13 @@
+//Name of the task: Use the parseInt Function
+
+//task: Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
+
+//solution:
+
+function convertToInteger(str) {
+
+	return parseInt(str)
+
+}
+
+convertToInteger("56");
