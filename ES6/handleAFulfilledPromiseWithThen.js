e4a76fa60a4436f7c187924911a268048ada91f4
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 14:54:22 +0300

    feat: add solution Handle a Fulfilled Promise with then

--- ES6/handleAFulfilledPromiseWithThen.js
+++ ES6/handleAFulfilledPromiseWithThen.js
@@ -15,3 +15,16 @@ myPromise.then(result => {
 // task: Add the then method to your promise. Use result as the parameter of its callback function and log result to the console.
 
 // solution:
+
+const makeServerRequest = new Promise((resolve, reject) => {
+
+	let responseFromServer = true;
+
+	if (responseFromServer) {
+		resolve("We got the data");
+	} else {
+		reject("Data not received");
+	}
+});
+
+makeServerRequest.then(() => console.log(2 + 2))
