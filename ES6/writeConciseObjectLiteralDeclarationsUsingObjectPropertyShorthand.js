a2fb53aec71bea06b01512344913bf9274347a4e
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:03:57 +0300

    feat: add solution Write Concise Object Literal Declarations Using Object Property Shorthand

--- ES6/writeConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand.js
+++ ES6/writeConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand.js
@@ -1,5 +1,15 @@
 //Name of the task: Write Concise Object Literal Declarations Using Object Property Shorthand
 
-//task: Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings. Use an iterator method (any kind of loop) to get the desired output (shown below).
+//task: Use object property shorthand with object literals to create and return an object with name, age and gender properties.
 
 //solution:
+
+const createPerson = (name, age, gender) => {
+	return {
+		name,
+		age,
+		gender,
+	};
+
+};
+createPerson("Zodiac Hasbro", 56, "male");
