5e7a38e3c370db15a3b57f06aae8091ef4a4740c
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:05:08 +0300

    init: start task Write Concise Declarative Functions with ES6

---
+++ ES6/writeConciseDeclarativeFunctionsWithES6.js
@@ -0,0 +1,5 @@
+//Name of the task: Write Concise Declarative Functions with ES6
+
+//task: Use object property shorthand with object literals to create and return an object with name, age and gender properties.
+
+//solution:
