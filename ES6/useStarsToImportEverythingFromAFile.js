b63618ab473dd5ba8d95415163b920e122eec747
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:45:06 +0300

    init: start task Use * to Import Everything from a File

---
+++ ES6/useStarsToImportEverythingFromAFile.js
@@ -0,0 +1,5 @@
+//Name of the task: Use * to Import Everything from a File
+
+//task: Add the appropriate import statement that will allow the current file to use the uppercaseString and lowercaseString functions you exported in the previous lesson. These functions are in a file called string_functions.js, which is in the same directory as the current file.
+
+//solution:
