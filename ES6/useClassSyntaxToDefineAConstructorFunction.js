2239833fd49753e64f01f3beaa5e6ec9017573b3
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:17:48 +0300

    feat: add solution Use class Syntax to Define a Constructor Function

--- ES6/useClassSyntaxToDefineAConstructorFunction.js
+++ ES6/useClassSyntaxToDefineAConstructorFunction.js
@@ -1,5 +1,16 @@
 //Name of the task: Use class Syntax to Define a Constructor Function
 
-//task: Refactor the function setGear inside the object bicycle to use the shorthand syntax described above.
+//task: Use the class keyword and write a constructor to create the Vegetable class. The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor.
 
 //solution:
+
+class Vegetable {
+	constructor(name) {
+		this.name = name
+	}
+}
+
+const carrot = new Vegetable('carrot');
+const pomodorro = new Vegetable('pomodorro')
+console.log(carrot.name);
+console.log(pomodorro.name);
