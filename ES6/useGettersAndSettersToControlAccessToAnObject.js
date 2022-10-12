2925eadfc0c1776f68f8a1138074b102c55183c7
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:21:30 +0300

    init: start task Use getters and setters to Control Access to an Object

--- ES6/useGettersAndSettersToControlAccessToAnObject.js
+++ ES6/useGettersAndSettersToControlAccessToAnObject.js
@@ -1,14 +1,5 @@
-//Name of the task: Use class Syntax to Define a Constructor Function
+//Name of the task: Use getters and setters to Control Access to an Object
 
 //task: Use the class keyword and write a constructor to create the Vegetable class. The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor.
 
 //solution:
-
-class Vegetable {
-	constructor(name) {
-		this.name = name
-	}
-}
-
-const carrot = new Vegetable('carrot');
-console.log(carrot.name);
