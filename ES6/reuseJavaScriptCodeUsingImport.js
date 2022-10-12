bd090a7914cf63fa1574b3091327ca7db26647ce
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:43:03 +0300

    feat: add solution Reuse JavaScript Code Using import

--- ES6/reuseJavaScriptCodeUsingImport.js
+++ ES6/reuseJavaScriptCodeUsingImport.js
@@ -1,5 +1,10 @@
 //Name of the task: Reuse JavaScript Code Using import
 
-//task: There are two string-related functions in the editor. Export both of them using the method of your choice.
+//task: Add the appropriate import statement that will allow the current file to use the uppercaseString and lowercaseString functions you exported in the previous lesson. These functions are in a file called string_functions.js, which is in the same directory as the current file.
 
 //solution:
+
+import { uppercaseString, lowercaseString } from './useExportToShareACodeBlock.js'
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
