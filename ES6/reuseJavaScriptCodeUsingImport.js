74999411ae563b2ac472b9a91f65e286c980d3f1
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:38:46 +0300

    init: start task Reuse JavaScript Code Using import

---
+++ ES6/reuseJavaScriptCodeUsingImport.js
@@ -0,0 +1,5 @@
+//Name of the task: Reuse JavaScript Code Using import
+
+//task: There are two string-related functions in the editor. Export both of them using the method of your choice.
+
+//solution:
