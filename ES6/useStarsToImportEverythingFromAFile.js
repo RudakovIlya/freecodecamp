8499a01212ccf79c45d0598154705bbfc2d1c357
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:46:47 +0300

    feat: add solution Use * to Import Everything from a File

--- ES6/useStarsToImportEverythingFromAFile.js
+++ ES6/useStarsToImportEverythingFromAFile.js
@@ -1,5 +1,11 @@
 //Name of the task: Use * to Import Everything from a File
 
-//task: Add the appropriate import statement that will allow the current file to use the uppercaseString and lowercaseString functions you exported in the previous lesson. These functions are in a file called string_functions.js, which is in the same directory as the current file.
+//task: The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. Use the import * as syntax to import everything from the file into an object called stringFunctions.
 
 //solution:
+
+import * as stringFunctions from './string_functions.js'
+
+stringFunctions.uppercaseString("hello");
+
+stringFunctions.lowercaseString("WORLD!");
