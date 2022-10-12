2923009c6e4d3a274966839a6e8ec5dbf7b48536
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 14:46:11 +0300

    feat: add solution Complete a Promise with resolve and reject

--- ES6/completeAPromiseWithResolveAndReject.js
+++ ES6/completeAPromiseWithResolveAndReject.js
@@ -16,6 +16,17 @@ const myPromise = new Promise((resolve, reject) => {
 
 //The example above uses strings for the argument of these functions, but it can really be anything. Often, it might be an object, that you would use data from, to put on your website or elsewhere.
 
-// task: Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
+// task: Make the promise handle success and failure. If responseFromServer is true, call the resolve method to successfully complete the promise. Pass resolve a string with the value We got the data. If responseFromServer is false, use the reject method instead and pass it the string: Data not received.
 
 // solution:
+
+const makeServerRequest = new Promise((resolve, reject) => {
+	// responseFromServer represents a response from a server
+	let responseFromServer;
+
+	if (responseFromServer) {
+		resolve('We got the data')
+	} else {
+		reject('Data not received')
+	}
+});
