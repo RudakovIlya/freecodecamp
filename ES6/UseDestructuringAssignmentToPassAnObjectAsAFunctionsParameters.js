858c0eff221ca4a64b64a998fd04233680e331f1
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 11:51:41 +0300

    test

--- ES6/UseDestructuringAssignmentToPassAnObjectAsAFunctionsParameters.js
+++ ES6/UseDestructuringAssignmentToPassAnObjectAsAFunctionsParameters.js
@@ -13,6 +13,6 @@ const stats = {
 	average: 35.85
 };
 
-// Only change code below this line
-const half = (stats) => (stats.max + stats.min) / 2.0;
- // Only change code above this line
+
+const half = ({ max, min }) => (max + min) / 2.0;
+
