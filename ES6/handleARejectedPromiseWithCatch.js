333e98949945eeebc0ff3464e4055375dabd93a9
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 14:59:49 +0300

    init: start task Handle a Rejected Promise with catch

---
+++ ES6/handleARejectedPromiseWithCatch.js
@@ -0,0 +1,17 @@
+// Name of the task: Handle a Rejected Promise with catch
+
+// theory: catch is the method used when your promise has been rejected. It is executed immediately after a promise's reject method is called. Here’s the syntax:
+
+/*
+
+myPromise.catch(error => {
+
+});
+
+*/
+
+// error is the argument passed in to the reject method.
+
+// task: Add the catch method to your promise. Use error as the parameter of its callback function and log error to the console.
+
+// solution:
