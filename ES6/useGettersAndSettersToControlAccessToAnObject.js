30e9ef33579255ce8209eaa675b3cc374be7def0
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:31:41 +0300

    feat: add solution Use getters and setters to Control Access to an Object

--- ES6/useGettersAndSettersToControlAccessToAnObject.js
+++ ES6/useGettersAndSettersToControlAccessToAnObject.js
@@ -1,5 +1,24 @@
 //Name of the task: Use getters and setters to Control Access to an Object
 
-//task: Use the class keyword and write a constructor to create the Vegetable class. The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor.
+//task: Use the class keyword to create a Thermostat class. The constructor accepts a Fahrenheit temperature.
 
 //solution:
+
+class Thermostat {
+	constructor(fahrenheit) {
+		this.fahrenheit = fahrenheit;
+	}
+
+	get temperature() {
+		return (5 / 9) * (this.fahrenheit - 32);
+	}
+
+	set temperature(celsius) {
+		this.fahrenheit = (celsius * 9.0) / 5 + 32;
+	}
+}
+
+const thermos = new Thermostat(76);
+let temp = thermos.temperature;
+thermos.temperature = 26;
+temp = thermos.temperature;
