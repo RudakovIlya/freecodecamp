5a0dd8b11ae4f5d2e43e977e5142420d31b69be6
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:09:18 +0300

    init: start task Use class Syntax to Define a Constructor Function

---
+++ ES6/useClassSyntaxToDefineAConstructorFunction.js
@@ -0,0 +1,5 @@
+//Name of the task: Use class Syntax to Define a Constructor Function
+
+//task: Refactor the function setGear inside the object bicycle to use the shorthand syntax described above.
+
+//solution:
