09933980f8a3179c4dae3da1511910aea9a59d04
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:53:26 +0300

    init: start task Create an Export Fallback with export default

---
+++ ES6/createAnExportFallbackWithExportDefault.js
@@ -0,0 +1,6 @@
+//Name of the task: Create an Export Fallback with export default
+
+//task: The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. Use the import * as syntax to import everything from the file into an object called stringFunctions.
+
+//solution:
+
