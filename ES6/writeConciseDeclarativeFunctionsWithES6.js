bc2f01eba8dcdbec608c94a1d3d0325a70520562
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 12:06:38 +0300

    feat: add solution Write Concise Declarative Functions with ES6

--- ES6/writeConciseDeclarativeFunctionsWithES6.js
+++ ES6/writeConciseDeclarativeFunctionsWithES6.js
@@ -1,5 +1,13 @@
 //Name of the task: Write Concise Declarative Functions with ES6
 
-//task: Use object property shorthand with object literals to create and return an object with name, age and gender properties.
+//task: Refactor the function setGear inside the object bicycle to use the shorthand syntax described above.
 
 //solution:
+
+
+const bicycle = {
+	gear: 2,
+	setGear(newGear) {
+		this.gear = newGear;
+	}
+};
