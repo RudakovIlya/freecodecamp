351b8b0d0789d84f9f41eb6d80848e43537e1e56
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-13 11:03:58 +0300

    feat: add solution Use the JavaScript Console to Check the Value of a Variable

--- Debugging/useTheJavaScriptConsoleToCheckTheValueOfAVariable.js
+++ Debugging/useTheJavaScriptConsoleToCheckTheValueOfAVariable.js
@@ -13,3 +13,11 @@ console.log('Hello world!');
 // task: Use the console.log() method to print the value of the variable a where noted in the code.
 
 // solution:
+
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a);
+
+let sumAB = a + b;
