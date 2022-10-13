4711d547c6d36b77cb6459e1bacbc603e547a978
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-13 11:55:12 +0300

    feat: add solution Access an Arrays Contents Using Bracket Notation

--- Basic Data Structures/accessAnArraysContentsUsingBracketNotation.js
+++ Basic Data Structures/accessAnArraysContentsUsingBracketNotation.js
@@ -34,3 +34,9 @@ ourArray[1] = "not b anymore";
 // task: In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
 
 // solution:
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = '2'
+// Only change code above this line
+console.log(myArray);
