4f1fb849aff5799b054151884b7c4b64940cdadf
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-13 12:16:06 +0300

    feat: add solution Remove Items from an Array with pop and shift

--- Basic Data Structures/removeItemsFromAnArrayWithPopAndShift.js
+++ Basic Data Structures/removeItemsFromAnArrayWithPopAndShift.js
@@ -33,3 +33,15 @@ let popped = greetings.pop();
 // task: We have defined a function, popShift, which takes an array as an argument and returns a new array. Modify the function, using pop() and shift(), to remove the first and last elements of the argument array, and assign the removed elements to their corresponding variables, so that the returned array contains their values.
 
 // solution:
+
+function popShift(arr) {
+
+	let popped = arr.pop()
+
+	let shifted = arr.shift()
+
+	return [shifted, popped];
+
+}
+
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
