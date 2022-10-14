5cf923ff650a71852cc3ad0dc05cfea365d580c9
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-14 13:15:31 +0300

    feat: add solution Modify an Object Nested Within an Object

--- Basic Data Structures/modifyAnObjectNestedWithinAnObject.js
+++ Basic Data Structures/modifyAnObjectNestedWithinAnObject.js
@@ -31,3 +31,16 @@ nestedObject.data.onlineStatus.busy = 10;
 // task: Here we've defined an object userActivity, which includes another object nested within it. Set the value of the online key to 45.
 
 // solution:
+
+let userActivity = {
+	id: 23894201352,
+	date: 'January 1, 2017',
+	data: {
+		totalUsers: 51,
+		online: 42
+	}
+};
+
+userActivity.data.online = 45;
+
+console.log(userActivity);
