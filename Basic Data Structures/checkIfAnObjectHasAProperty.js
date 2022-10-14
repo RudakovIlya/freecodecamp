a51da2e20ef3f2dd98fb203a5df46076faf55daa
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-14 13:31:19 +0300

    init: start task Check if an Object has a Property

---
+++ Basic Data Structures/checkIfAnObjectHasAProperty.js
@@ -0,0 +1,16 @@
+// Name of the task: Check if an Object has a Property
+
+// theory: Now we can add, modify, and remove keys from objects. But what if we just wanted to know if an object has a specific property? JavaScript provides us with two different ways to do this. One uses the hasOwnProperty() method and the other uses the in keyword. If we have an object users with a property of Alan, we could check for its presence in either of the following ways:
+
+/*
+
+users.hasOwnProperty('Alan');
+'Alan' in users;
+
+*/
+
+// Both of these would return true.
+
+// task: Use the delete keyword to remove the oranges, plums, and strawberries keys from the foods object.
+
+// solution:
