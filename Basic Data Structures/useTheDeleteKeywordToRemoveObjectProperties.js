bf6075fb4ff96bfc720b6d644d80ed2c4c7e7217
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-14 13:26:05 +0300

    feat: add solution Use the delete Keyword to Remove Object Properties

--- Basic Data Structures/useTheDeleteKeywordToRemoveObjectProperties.js
+++ Basic Data Structures/useTheDeleteKeywordToRemoveObjectProperties.js
@@ -12,6 +12,24 @@ delete foods.apples;
 
 */
 
-// task: We've defined a function, checkInventory, which receives a scanned item as an argument. Return the current value of the scannedItem key in the foods object. You can assume that only valid keys will be provided as an argument to checkInventory.
+// task: Use the delete keyword to remove the oranges, plums, and strawberries keys from the foods object.
 
 // solution:
+
+let foods = {
+	apples: 25,
+	oranges: 32,
+	plums: 28,
+	bananas: 13,
+	grapes: 35,
+	strawberries: 27
+};
+
+delete foods['oranges'];
+
+delete foods['plums'];
+
+delete foods['strawberries'];
+
+
+console.log(foods);
