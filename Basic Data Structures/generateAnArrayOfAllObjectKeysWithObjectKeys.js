a475433f4fa67b1d4195051ce396ca64308f3fb2
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-14 13:42:10 +0300

    init: start task Generate an Array of All Object Keys with Object.keys()

---
+++ Basic Data Structures/generateAnArrayOfAllObjectKeysWithObjectKeys.js
@@ -0,0 +1,7 @@
+// Name of the task: Generate an Array of All Object Keys with Object.keys()
+
+// theory: We can also generate an array which contains all the keys stored in an object with the Object.keys() method. This method takes an object as the argument and returns an array of strings representing each property in the object. Again, there will be no specific order to the entries in the array.
+
+// task: Finish writing the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument.
+
+// solution:
