1e6be196398dc6b260e3e6ea15cbb11e6e10a3c7
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-15 12:18:44 +0300

    feat: add solution Factorialize a Number

--- Basic Algorithm Scripting/factorializeANumber.js
+++ Basic Algorithm Scripting/factorializeANumber.js
@@ -1,9 +1,59 @@
 //Name of the task: Factorialize a Number
 
-//task: Reverse the provided string.
+//task: Return the factorial of the provided integer.
 
-// You may need to turn the string into an array before you can reverse it.
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
 
-// Your result must be a string.
+// Factorials are often represented with the shorthand notation n!
+
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+// Only integers greater than or equal to zero will be supplied to the function.
 
 //solution:
+
+function factorialize(num) {
+
+	if (num < 1) {
+
+		return 1;
+
+	}
+
+	return num * factorialize(num - 1);
+
+}
+
+factorialize(10);
+
+function factorialize(num) {
+
+	let result = 1;
+
+	for (let i = 1; i <= num; i++) {
+
+		result *= i;
+
+	}
+
+	return result;
+
+}
+
+console.log(factorialize(5));
+
+function factorialize(num) {
+
+	let result = 1;
+
+	while (num) {
+
+		result *= n--;
+
+	}
+
+	return result;
+
+}
+
+console.log(factorialize(5));
