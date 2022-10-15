477e89083ee3b513f001be1785d90aa9bd08a4e4
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-15 12:47:53 +0300

    feat: add task description Repeat a String Repeat a String

---
+++ Basic Algorithm Scripting/repeatAStringRepeatAString.js
@@ -0,0 +1,5 @@
+//Name of the task: Repeat a String Repeat a String
+
+//task: Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+
+//solution:
