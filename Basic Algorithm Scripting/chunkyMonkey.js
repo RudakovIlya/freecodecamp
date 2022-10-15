c14af07fb1518121f59b00df53a0a96febc31d48
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-15 14:30:12 +0300

    feat: add solution Chunky Monkey

--- Basic Algorithm Scripting/chunkyMonkey.js
+++ Basic Algorithm Scripting/chunkyMonkey.js
@@ -3,3 +3,24 @@
 //task: Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
 //solution:
+
+function chunkArrayInGroups(arr, size) {
+
+	const newArr = [];
+
+	while (arr.length) {
+
+		newArr.push(arr.splice(0, size))
+
+	}
+
+	return newArr
+
+}
+//console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2))
+//console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3))
+//console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2))
+//console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4))
+//console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3))
+//console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4))
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)) 
