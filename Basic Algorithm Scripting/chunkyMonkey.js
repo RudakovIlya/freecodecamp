ce91ceb3bcb067e3c3f4de8b8bb28090ac80df9a
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-15 14:08:07 +0300

    feat: add task description Chunky Monkey

---
+++ Basic Algorithm Scripting/chunkyMonkey.js
@@ -0,0 +1,5 @@
+//Name of the task: Chunky Monkey
+
+//task: Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+//solution:
