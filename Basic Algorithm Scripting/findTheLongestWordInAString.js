b5e15f4e022d3819a4998c3847d05bd60d9d3bf6
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-15 12:21:19 +0300

    feat: add task description Find the Longest Word in a String

---
+++ Basic Algorithm Scripting/findTheLongestWordInAString.js
@@ -0,0 +1,7 @@
+//Name of the task: Find the Longest Word in a String
+
+//task: Return the length of the longest word in the provided sentence.
+
+// Your response should be a number.
+
+//solution:
