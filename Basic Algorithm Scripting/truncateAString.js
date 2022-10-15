cb01ad1353b100b641a39a82b5085a316d3d5974
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-15 13:05:06 +0300

    feat: add solution Truncate a String

--- Basic Algorithm Scripting/truncateAString.js
+++ Basic Algorithm Scripting/truncateAString.js
@@ -3,3 +3,11 @@
 //task: Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
 
 //solution:
+
+function truncateString(str, num) {
+
+	return str.length > num ? `${str.slice(0, num)}...` : str;
+
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
