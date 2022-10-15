315bd80cff1d88cf630596a8c3431e8013775ef5
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-15 12:10:12 +0300

    feat: add task description Factorialize a Number

---
+++ Basic Algorithm Scripting/factorializeANumber.js
@@ -0,0 +1,9 @@
+//Name of the task: Factorialize a Number
+
+//task: Reverse the provided string.
+
+// You may need to turn the string into an array before you can reverse it.
+
+// Your result must be a string.
+
+//solution:
