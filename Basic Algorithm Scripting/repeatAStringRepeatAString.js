9914493683ebdb1f16d0361837abc3315e6f52d1
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-15 12:58:28 +0300

    feat: add solution description Repeat a String Repeat a String

--- Basic Algorithm Scripting/repeatAStringRepeatAString.js
+++ Basic Algorithm Scripting/repeatAStringRepeatAString.js
@@ -3,3 +3,40 @@
 //task: Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
 
 //solution:
+
+function repeatStringNumTimes(str, num) {
+
+	if (num < 1) {
+
+		return '';
+
+	}
+
+	return str + repeatStringNumTimes(str, num - 1);
+
+}
+
+console.log(repeatStringNumTimes("abc", 3));
+
+function repeatStringNumTimes(str, num) {
+
+	return num > 0 ? str + repeatStringNumTimes(str, num - 1) : '';
+
+}
+
+console.log(repeatStringNumTimes("abc", 3));
+
+function repeatStringNumTimes(str, num) {
+
+	let string = "";
+
+	for (let i = 0; i < num; i++) {
+
+		string += str;
+
+	}
+
+	return string;
+}
+
+console.log(repeatStringNumTimes("abc", 3));
