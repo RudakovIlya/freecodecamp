c530fe0d9ba4237ffe3952c898759ade5d14b8a2
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-15 12:26:46 +0300

    feat: add task description Return Largest Numbers in Arrays

---
+++ Basic Algorithm Scripting/returnLargestNumbersInArrays.js
@@ -0,0 +1,7 @@
+//Name of the task: Return Largest Numbers in Arrays
+
+//task: Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+// Your response should be a number.
+
+//solution:
