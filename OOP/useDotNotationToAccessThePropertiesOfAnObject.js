c38159c46d9b03431ade2aabef0755dde9a42b35
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-16 11:09:15 +0300

    feat: add task theory and description Use Dot Notation to Access the Properties of an Object

---
+++ OOP/useDotNotationToAccessThePropertiesOfAnObject.js
@@ -0,0 +1,19 @@
+//Name of the task: Use Dot Notation to Access the Properties of an Object
+
+// theory: The last challenge created an object with various properties. Now you'll see how to access the values of those properties. Here's an example:
+
+/*
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2
+};
+console.log(duck.name);
+
+This duck object has two property/value pairs: a name of Aflac and a numLegs of 2.
+
+*/
+
+//task: Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
+
+//solution:
