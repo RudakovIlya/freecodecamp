c372a2a1cfedaf29cd69ef43ddf44d4e1b13b206
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-17 16:03:31 +0300

    feat: add solution Use Closure to Protect Properties Within an Object from Being Modified Externally

--- OOP/useClosureToProtectPropertiesWithinAnObjectFromBeingModifiedExternally.js
+++ OOP/useClosureToProtectPropertiesWithinAnObjectFromBeingModifiedExternally.js
@@ -32,3 +32,14 @@ ducky.getHatchedEggCount();
 // task: Change how weight is declared in the Bird function so it is a private variable. Then, create a method getWeight that returns the value of weight 15..
 
 //solution:
+
+function Bird() {
+	let weight = 15;
+
+	this.getWeight = () => {
+
+		return weight;
+
+	}
+
+}
