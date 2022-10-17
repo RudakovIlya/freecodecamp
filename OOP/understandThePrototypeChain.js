9a7e744bae00ac716b167cea6c38e9fa05447de1
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-17 13:26:44 +0300

    feat: add solution Understand the Prototype Chain

--- OOP/understandThePrototypeChain.js
+++ OOP/understandThePrototypeChain.js
@@ -39,12 +39,13 @@ duck.hasOwnProperty("name");
 //solution:
 
 function Dog(name) {
+
 	this.name = name;
+
 }
 
 let beagle = new Dog("Snoopy");
 
 Dog.prototype.isPrototypeOf(beagle);  // yields true
 
-// Fix the code below so that it evaluates to true
 Object.prototype.isPrototypeOf(Dog.prototype);
