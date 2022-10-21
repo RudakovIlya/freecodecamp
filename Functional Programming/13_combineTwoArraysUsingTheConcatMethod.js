be95a02b14cada6c41048820353d822918025c9b
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-21 17:05:36 +0300

    feat: add solution Combine Two Arrays Using the concat Method

--- Functional Programming/13_combineTwoArraysUsingTheConcatMethod.js
+++ Functional Programming/13_combineTwoArraysUsingTheConcatMethod.js
@@ -13,3 +13,15 @@ The returned array would be [1, 2, 3, 4, 5, 6].
 // task: Use the concat method in the nonMutatingConcat function to concatenate attach to the end of original. The function should return the concatenated array.
 
 //solution:
+
+function nonMutatingConcat(original, attach) {
+	// Only change code below this line
+
+	return original.concat(attach);
+
+	// Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
