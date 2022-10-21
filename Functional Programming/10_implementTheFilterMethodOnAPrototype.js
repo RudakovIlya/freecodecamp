31058a900351f58d4183fa7341dc5bd28b4adfb1
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-21 16:42:23 +0300

    feat: add solution Implement the filter Method on a Prototype

--- Functional Programming/10_implementTheFilterMethodOnAPrototype.js
+++ Functional Programming/10_implementTheFilterMethodOnAPrototype.js
@@ -9,3 +9,22 @@ theory: You might learn a lot about the filter method if you implement your own 
 // task: Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You should not use the built-in filter method. The Array instance can be accessed in the myFilter method using this.
 
 //solution:
+
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function (callback) {
+	// Only change code below this line
+	const newArray = [];
+
+	this.forEach(a => callback(a) ? newArray.push(a) : null)
+
+	// Only change code above this line
+	return newArray;
+};
+
+const new_s = s.myFilter(function (item) {
+	return item % 2 === 1;
+});
+
+console.log(new_s);
