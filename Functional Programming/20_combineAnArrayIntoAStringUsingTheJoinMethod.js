6035ff79e947491d72f710be094d5fcd6d05c7cb
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-21 19:00:00 +0300

    feat: add solution Combine an Array into a String Using the join Method

--- Functional Programming/20_combineAnArrayIntoAStringUsingTheJoinMethod.js
+++ Functional Programming/20_combineAnArrayIntoAStringUsingTheJoinMethod.js
@@ -15,3 +15,11 @@ str would have a value of the string Hello World.
 // task: Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. The function should return a string. For example, I-like-Star-Wars would be converted to I like Star Wars. For this challenge, do not use the replace method.
 
 // solution:
+
+function sentensify(str) {
+	// Only change code below this line
+
+	return str.split(/\W/).join(' ');
+
+	// Only change code above this line
+}
