1f8df164dc92b95a52dd3476c62b65ff2c8a567a
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-21 15:18:51 +0300

    feat: add soluiton Refactor Global Variables Out of Functions

--- Functional Programming/06_refactorGlobalVariablesOutOfFunctions.js
+++ Functional Programming/06_refactorGlobalVariablesOutOfFunctions.js
@@ -15,3 +15,31 @@ Adding one to a number is not very exciting, but we can apply these principles w
 // task: Rewrite the code so the global array bookList is not changed inside either function. The add function should add the given bookName to the end of the array passed to it and return a new array (list). The remove function should remove the given bookName from the array passed to it. Note: Both functions should return an array, and any new parameters should be added before the bookName parameter.
 
 //solution:
+
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+function add(arr, bookName) {
+
+	let newArr = [...arr];
+
+	newArr.push(bookName);
+
+	return newArr;
+
+}
+
+function remove(arr, bookName) {
+
+	let newArr = [...arr];
+
+	const book_index = newArr.indexOf(bookName);
+
+	if (book_index >= 0) {
+
+		newArr.splice(book_index, 1);
+
+		return newArr;
+
+	}
+
+}
