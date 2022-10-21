ae249db69e13ad7ff7928ddc4e4fbfcdd3d6ea50
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-21 16:35:34 +0300

    init: start task Implement the filter Method on a Prototype

--- Functional Programming/10_implementTheFilterMethodOnAPrototype.js
+++ Functional Programming/10_implementTheFilterMethodOnAPrototype.js
@@ -6,6 +6,6 @@ theory: You might learn a lot about the filter method if you implement your own 
 
 */
 
-// task: The variable watchList holds an array of objects with information on several movies. Use a combination of filter and map on watchList to assign a new array of objects with only title and rating keys. The new array should only include objects where imdbRating is greater than or equal to 8.0. Note that the rating values are saved as strings in the object and you may need to convert them into numbers to perform mathematical operations on them.
+// task: Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You should not use the built-in filter method. The Array instance can be accessed in the myFilter method using this.
 
 //solution:
