2b487cc51fa2b8f7e8bc0293e6dc7014f0491c66
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-22 11:35:47 +0300

    docs: the task is not solved

--- Intermediate Algorithm Scripting/05_spinalTapCase.js
+++ Intermediate Algorithm Scripting/05_spinalTapCase.js
@@ -3,3 +3,18 @@
 /* task: Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes. */
 
 // solution: 
+
+function spinalCase(str) {
+	return str.split(' ').map(item => {
+
+		if (item.toUpperCase() == item) {
+
+			return ` ${item}`;
+
+		}
+		return item
+	}).join(' ').split(/\W/).map(item => item.toLowerCase()).join('-')
+}
+
+console.log(spinalCase('This Is Spinal Tap'));
+
