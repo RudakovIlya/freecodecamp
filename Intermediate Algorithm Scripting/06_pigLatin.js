9bfe12356bb9cc2286c0cf74047694a087837a90
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-23 12:30:09 +0300

    fix

--- Intermediate Algorithm Scripting/06_pigLatin.js
+++ Intermediate Algorithm Scripting/06_pigLatin.js
@@ -19,37 +19,3 @@ function translatePigLatin(str) {
 }
 
 translatePigLatin("consonant");
-
-function uniteUnique(...arr) {
-
-	const newArr = [];
-
-	for (let i = 0; i < arr.length; i++) {
-
-		for (let k = 0; k < arr[i].length; k++) {
-
-			newArr.push(arr[i][k])
-
-		}
-
-	}
-
-	return Array.from(new Set(newArr))
-}
-
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
-
-function uniteUnique(...arr) {
-
-	return Array.from(new Set(arr.reduce((accum, item) => {
-
-		accum.push(...item)
-
-		return accum
-
-	}, [])))
-}
-
-const uniteUnique = (...arr) => [...new Set(arr.flat(1))]
-
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
