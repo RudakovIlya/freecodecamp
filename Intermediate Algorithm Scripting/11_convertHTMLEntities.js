91336fd053ee907e0e0274e372f5790cf74c0766
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-24 19:44:34 +0300

    init: start task Convert HTML Entities

---
+++ Intermediate Algorithm Scripting/11_convertHTMLEntities.js
@@ -0,0 +1,5 @@
+// Name of the task: Convert HTML Entities
+
+/* task: Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities. */
+
+// solution: 
