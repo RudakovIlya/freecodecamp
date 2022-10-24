d5a959fa45de0ca56f185b6aa7b8f3c9560a68aa
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-24 19:49:33 +0300

    feat: add solution Convert HTML Entities

--- Intermediate Algorithm Scripting/11_convertHTMLEntities.js
+++ Intermediate Algorithm Scripting/11_convertHTMLEntities.js
@@ -3,3 +3,9 @@
 /* task: Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities. */
 
 // solution: 
+
+function convertHTML(str) {
+	return str.replace(/&/g, '&amp;').replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(/\"/g, '&quot;').replace(/\'/g, '&apos;');
+}
+
+console.log(convertHTML("Schindler's List"));
