339e74c9cd6f5e6243093d407bc29ce3c553674f
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-24 17:05:36 +0300

    init: start Missing letters

---
+++ Intermediate Algorithm Scripting/09_missingLetters.js
@@ -0,0 +1,7 @@
+// Name of the task: Missing letters
+
+/* task: Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined. */
+
+// solution: 
