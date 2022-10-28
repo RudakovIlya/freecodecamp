bf7ceaa6cf274d012decbe2ad9686561c7874f91
ilya <rudakov.alhan@gmail.com>
2022-10-28 12:07:33 +0300

    feat: add soluiton DNA Pairing

--- Intermediate Algorithm Scripting/08_DNAPairing.js
+++ Intermediate Algorithm Scripting/08_DNAPairing.js
@@ -11,24 +11,24 @@ The character and its pair are paired up in an array, and all the arrays are gro
 // solution: 
 
 function pairElement(str) {
-	const obj = Object.entries({
+	const obj = {
 		A: 'T',
 		T: 'A',
 		C: 'G',
 		G: 'C'
-	})
+	};
 	const array = str.split('');
 
 	const newArray = [];
 
 	for (let i = 0; i < array.length; i++) {
-
-		if (array[i] === obj[i]['0']) {
-			newArray.push(obj[i])
+		if (obj.hasOwnProperty(array[i])) {
+			newArray.push([array[i], obj[array[i]]])
 		}
-
 	}
 	return newArray
 }
 
-console.log(pairElement("CTCTA"));
+console.log(pairElement("ATCGA")); //should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+console.log(pairElement("TTGAG")); //should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+console.log(pairElement("CTCTA")); //should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
