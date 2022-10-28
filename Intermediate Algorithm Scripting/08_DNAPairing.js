ec025437b9e00baaca747037cfa9d921fa29ae33
ilya <rudakov.alhan@gmail.com>
2022-10-28 12:09:12 +0300

    feat: add new soluiton

--- Intermediate Algorithm Scripting/08_DNAPairing.js
+++ Intermediate Algorithm Scripting/08_DNAPairing.js
@@ -29,6 +29,17 @@ function pairElement(str) {
 	return newArray
 }
 
+function pairElement(str) {
+	const obj = {
+		A: 'T',
+		T: 'A',
+		C: 'G',
+		G: 'C'
+	};
+
+	return str.split('').map(item => [item, obj[item]])
+}
+
 console.log(pairElement("ATCGA")); //should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
 console.log(pairElement("TTGAG")); //should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
 console.log(pairElement("CTCTA")); //should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
