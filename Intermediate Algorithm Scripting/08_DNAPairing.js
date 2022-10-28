085e4dec1ec240b8e64cfe29d24974268e057d84
ilya <rudakov.alhan@gmail.com>
2022-10-28 12:02:09 +0300

    test

--- Intermediate Algorithm Scripting/08_DNAPairing.js
+++ Intermediate Algorithm Scripting/08_DNAPairing.js
@@ -9,3 +9,26 @@ For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
 The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array. */
 
 // solution: 
+
+function pairElement(str) {
+	const obj = Object.entries({
+		A: 'T',
+		T: 'A',
+		C: 'G',
+		G: 'C'
+	})
+	const array = str.split('');
+
+	const newArray = [];
+
+	for (let i = 0; i < array.length; i++) {
+
+		if (array[i] === obj[i]['0']) {
+			newArray.push(obj[i])
+		}
+
+	}
+	return newArray
+}
+
+console.log(pairElement("CTCTA"));
