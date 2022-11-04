5e252bd8a5c2b0717bbd34f5d54c00324063291d
ilya <rudakov.alhan@gmail.com>
2022-11-04 18:20:24 +0300

    feat: add solution task Match Literal Strings

--- RegExp/02_matchLiteralStrings.js
+++ RegExp/02_matchLiteralStrings.js
@@ -22,3 +22,7 @@ A future challenge will show how to match those other forms as well.
 // task: Complete the regex waldoRegex to find "Waldo" in the string waldoIsHiding with a literal match.
 
 //solution:
+
+let waldoIsHiding = 'Somewhere Waldo is hiding in this text.';
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
